4b1bc5edbd00cf91aa6dcea11b3c32f6a916a50c
Victor Ukaegbu <ikwuonwuvictor@gmail.com>
2025-12-19 10:16:19 +0100

    Add Docker support for backend application
    - Created Dockerfile for building the Node.js application.
    - Added docker-compose.yml for managing backend and MongoDB services.
    - Introduced .dockerignore to exclude unnecessary files from the Docker context.
    - Updated package.json with a new build script.
    - Refactored import paths in app.js for consistency.

--- Backend/config/app.js
+++ Backend/config/app.js
@@ -3,36 +3,22 @@ import cors from "cors"
 import { whitelist } from "./defaults.js";
 import { errorHandler, notFound } from "../middlewares/errorHandler.js";
 import helmet from "helmet";
-import authRoutes from "../Auth/AuthRoutes.js"
+import authRoutes from "../Auth/authRoutes.js"
 import userRoutes from "../Users/userRoutes.js"
 import announcementRoutes from "../Announcement/AnnouncementRoutes.js"
 import assignmentRoutes from "../Assignment/AssignmentRoutes.js"
 import eventRoutes from "../Event/EventRoutes.js"
-import timetableRoutes from "../Timetable/TimetableRoutes.js"
+import timetableRoutes from "../Timetable/timetableRoutes.js"
 import { timeoutMiddleware, haltOnTimedout } from '../middlewares/timeoutMiddleware.js';
 import mongoSanitise from 'express-mongo-sanitize'
-import swaggerUi from 'swagger-ui-express'
-import YAML from 'yamljs'
-import path from 'path'
-import { fileURLToPath } from 'url'
+
 
 const app = express()
 
-// Define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// This line creates the correct path to your openapi.yaml file
-const swaggerDocument = YAML.load(path.join(__dirname, '../api/openapi.yaml'));
-
-//middlewares
-
-// This is the new route for your interactive docs.
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(helmet())
 app.use(cors({
     origin: whitelist,
-    Credentials: true
+    credentials: true
 }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
